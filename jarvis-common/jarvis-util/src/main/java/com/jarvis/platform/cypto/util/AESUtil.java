ae7d330ecc4776518cbb17393e5e92c4a4f45ada
Doug Wang <wangtao@deehero.com>
2023-02-08 12:49:36 +0800

    Initial commit

---
+++ jarvis-common/jarvis-util/src/main/java/com/jarvis/platform/cypto/util/AESUtil.java
@@ -0,0 +1,11 @@
+package com.jarvis.platform.cypto.util;
+
+/**
+ * AES加密解密
+ *
+ * @author qiucs
+ * @version 1.0.0 2021年9月2日
+ */
+public class AESUtil extends com.jarvis.framework.cypto.util.AESUtil {
+
+}
