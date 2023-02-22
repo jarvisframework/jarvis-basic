dd631909d91c229895097ab00f874bd2de475726
Doug Wang <wangtao@deehero.com>
2023-02-22 12:22:57 +0800

    Initial commit

--- jarvis-common/jarvis-eureka-client/src/main/java/com/jarvis/platform/cloud/util/ArchiveUtilAutoConfiguration.java
+++ jarvis-common/jarvis-eureka-client/src/main/java/com/jarvis/platform/cloud/util/ArchiveUtilAutoConfiguration.java
@@ -8,18 +8,8 @@ import org.springframework.cloud.commons.util.InetUtilsProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.commons.util.InetUtils;
-import org.springframework.cloud.commons.util.InetUtilsProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 /**
- *
- * @author qiucs
+ * @author Doug Wang
  * @version 1.0.0 2022年8月1日
  */
 @Configuration(proxyBeanMethods = false)
--- jarvis-common/jarvis-util/src/main/java/com/jarvis/platform/cypto/util/AESUtil.java
+++ jarvis-common/jarvis-util/src/main/java/com/jarvis/platform/cypto/util/AESUtil.java
@@ -3,7 +3,7 @@ package com.jarvis.platform.cypto.util;
 /**
  * AES加密解密
  *
- * @author qiucs
+ * @author Doug Wang
  * @version 1.0.0 2021年9月2日
  */
 public class AESUtil extends com.jarvis.framework.cypto.util.AESUtil {
